8a1642595f125eb9fd0891b50731f25052ce38a5
Sam Millows <sammillows@mac.localdomain>
2025-01-04 12:32:51 +0100

    Add Delete route

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import json
 from flask import Flask, render_template, request, redirect, url_for
 
-
 app = Flask(__name__)
-
 
 # load blog posts from JSON
 def load_blog_posts():
@@ -13,7 +11,7 @@ def load_blog_posts():
 @app.route('/')
 def index():
     blog_posts = load_blog_posts()
-    return render_template('index.html', posts = blog_posts)
+    return render_template('index.html', posts=blog_posts)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -31,11 +29,25 @@ def add():
 
         # save the updated list to JSON file
         with open("data/blog_posts.json", "w") as file:
-            json.dump(blog_posts, file, indent = 4)
+            json.dump(blog_posts, file, indent=4)
 
         return redirect(url_for('index'))
 
     return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
+    blog_posts = load_blog_posts()
+
+    # Remove the post with the matching ID
+    blog_posts = [post for post in blog_posts if post['id'] != post_id]
+
+    # Save the updated list back to the JSON file
+    with open("data/blog_posts.json", "w") as file:
+        json.dump(blog_posts, file, indent=4)
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
