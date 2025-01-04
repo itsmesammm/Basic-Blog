e841fe39e948150b4f459a461d19d50d48a004bb
Sam Millows <sammillows@mac.localdomain>
2025-01-04 12:58:57 +0100

    Add Update route

--- app.py
+++ app.py
@@ -49,6 +49,29 @@ def delete(post_id):
 
     return redirect(url_for('index'))
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    blog_posts = load_blog_posts()
+
+    # find the post by its ID
+    post = next((p for p in blog_posts if p['id'] == post_id), None)
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        # Update the blog post with the new data from the form
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+
+        # Save the updated list back to the JSON file
+        with open("data/blog_posts.json", "w") as file:
+            json.dump(blog_posts, file, indent = 4)
+
+        return redirect(url_for('index'))
+
+    # If its a GET request, render the update form
+    return render_template('update.html', post=post)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
